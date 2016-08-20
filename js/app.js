b1b2f49737cc91e1952643692d91733554c74938
Yuan Chen <jerry@crossnow.io>
2016-08-20 17:05:16 -0400

    indico api

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-angular.module('chatHelperApp', [])
-  .controller('mainController', ['$scope',function($scope) {
-  	$scope.twitter_handle = "@";
 
-  	$scope.submitTwitterHandle = function(){
-  		
-  	}
-  }]);
+var app = angular.module('ChatHelperApp', []);
+
+app.controller('MainController', ['$scope','indico', function($scope, indico) {
+
+	$scope.twitter_handle = "@";
+
+	$scope.submitTwitterHandle = function(){
+
+		// PLACEHOLDER
+		text = "Oh oh oh oh oh oh, Oh oh oh oh oh oh. You think I'm funny when I tell the punchline wrong. You just gotta ignite the light and let it shine! Never planned that one day I'd be losing you. Boy, you should know what you're falling for. Last Friday night, yeah I think we broke the law, always say we're gonna stop. You could travel the world but nothing comes close to the golden coast.Are you ready for, ready for. All night they're playing, your song. Saw you downtown singing the Blues. We drove to Cali and got drunk on the beach. You think I'm pretty without any makeup on. Be your teenage dream tonight. Come on, show 'em what you're worth.Think the city towed my car, chandeliers on the floor. Yeah, she's so in demand. You're reading me like erotica, boy, you make me feel exotic, yeah. I'll be your gift, give you something good to celebrate. Stinging like a bee I earned my stripes. What's up your sleeve, such a tease. She eats your heart out. Like Jeffrey Dahmer (woo).";
+		indico.getTextTags(text);
+	}
+
+}]).config(function ($httpProvider) { // FOR LOCAL TESTING
+	$httpProvider.defaults.headers.common = {};
+	$httpProvider.defaults.headers.post = {};
+	$httpProvider.defaults.headers.put = {};
+	$httpProvider.defaults.headers.patch = {};
+});;
---
+++ js/services/indico.js
@@ -0,0 +1,103 @@
+angular.module('ChatHelperApp').
+	factory('indico', ['$http', function($http) {
+
+	var api_key = 'f844248f8582881cdf65b74c1718d0ac',
+		api_url_tags = 'https://apiv2.indico.io/texttags?version=2',
+		api_url_personality = 'https://apiv2.indico.io/keywords?version=2',
+		api_url_political = 'https://apiv2.indico.io/keywords?version=2';
+
+
+	function getPolitical(text){
+		$http.post(
+			api_url_political,
+			JSON.stringify({
+				'api_key': api_key,
+				'data': text,
+				'threshold': 0.1
+			})
+		).then(function(res) {
+			var word_array = [];
+			var result = res.data.results;
+
+			console.log(result);
+
+			for (var key in result) {
+				word_array.push({
+					word: key,
+					value: result[key]
+				});
+			}
+
+			word_array.sort(function(a, b){return b.value - a.value});
+			console.log(word_array);
+			return word_array;
+		}, function(error){
+			console.log(error);
+		});
+	}
+
+	function getPersonality(text){
+		$http.post(
+			api_url_personality,
+			JSON.stringify({
+				'api_key': api_key,
+				'data': text,
+				'threshold': 0.1
+			})
+		).then(function(res) {
+			var word_array = [];
+			var result = res.data.results;
+
+			console.log(result);
+
+			for (var key in result) {
+				word_array.push({
+					word: key,
+					value: result[key]
+				});
+			}
+
+			word_array.sort(function(a, b){return b.value - a.value});
+			console.log(word_array);
+			return word_array;
+		}, function(error){
+			console.log(error);
+		});
+	}
+
+	function getTextTags(text){
+		$http.post(
+			api_url_tags,
+			JSON.stringify({
+				'api_key': api_key,
+				'data': text,
+				'threshold': 0.1
+			})
+		).then(function(res) {
+			var word_array = [];
+			var result = res.data.results;
+
+			console.log(result);
+
+			for (var key in result) {
+				word_array.push({
+					word: key,
+					value: result[key]
+				});
+			}
+
+			word_array.sort(function(a, b){return b.value - a.value});
+			console.log(word_array);
+			return word_array;
+		}, function(error){
+			console.log(error);
+		});
+	}
+	function getAnalysis(text){
+
+	}
+
+	return {
+		getTextTags: getTextTags
+	}
+}]);
